f43386b896ebd389dcffe00413b4eefb6d546178
Denis Zinger <mamontinnokent@gmail.com>
2021-06-24 11:21:09 +0300

    update 0.1

---
+++ server/src/main/java/com/example/olympiads/OlympiadsApplication.java
@@ -0,0 +1,13 @@
+package com.example.olympiads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympiadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlympiadsApplication.class, args);
+    }
+
+}
